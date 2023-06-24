112bee6a54b649beb3108034a59bb932355e2241
justin0979 <justinm0979@gmail.com>
2023-06-24 18:39:48 -0500

    add properties

--- src/store/slices/usersSlice.ts
+++ src/store/slices/usersSlice.ts
@@ -2,16 +2,21 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface UsersType {
   name: string;
+  id?: number;
 }
 
 interface UsersState {
   data: UsersType[];
+  isLoading: boolean;
+  error: Object | null;
 }
 
 const usersSlice = createSlice({
   name: "users",
   initialState: {
     data: [],
+    isLoading: false,
+    error: null,
   } as UsersState,
   reducers: {},
 });
