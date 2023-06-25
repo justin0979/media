47d4bc2efe3d11ac932b524e3f44d7073173baad
justin0979 <justinm0979@gmail.com>
2023-06-25 13:49:18 -0500

    display status of requests

--- src/components/UsersList.tsx
+++ src/components/UsersList.tsx
@@ -1,17 +1,31 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { AppDispatch, fetchUsers } from "../store";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, UsersState, fetchUsers } from "../store";
 
 function UsersList() {
   const dispatch = useDispatch<AppDispatch>();
+  const { isLoading, error, data } = useSelector(
+    (state: { users: UsersState }) => {
+      return state.users;
+    },
+  );
 
   useEffect(() => {
     dispatch(fetchUsers());
   }, []);
 
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error fetching data...</div>;
+  }
+
   return (
     <div>
       <h1>Users List</h1>
+      {data.length}
     </div>
   );
 }
