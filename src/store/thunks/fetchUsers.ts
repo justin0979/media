9f7465ba418c9f3552823a763b2358d0c37c425a
justin0979 <justinm0979@gmail.com>
2023-06-25 13:54:44 -0500

    add pause function for development use only

--- src/store/thunks/fetchUsers.ts
+++ src/store/thunks/fetchUsers.ts
@@ -5,7 +5,17 @@ import { UsersType } from "../slices";
 const fetchUsers = createAsyncThunk("users/fetch", async () => {
   const response = await axios.get("http://localhost:3005/users");
 
+  // DEV ONLY
+  await pause(1000);
+
   return response.data as UsersType[];
 });
+
+// DEV ONLY
+const pause = (duration: number) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, duration);
+  });
+};
 
 export { fetchUsers };
