e87094cb4b1e201286734398957d0627620ca5d7
justin0979 <justinm0979@gmail.com>
2023-06-25 13:42:18 -0500

    hook up fetchUsers thunk

--- src/components/UsersList.tsx
+++ src/components/UsersList.tsx
@@ -1,4 +1,14 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { AppDispatch, fetchUsers } from "../store";
+
 function UsersList() {
+  const dispatch = useDispatch<AppDispatch>();
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, []);
+
   return (
     <div>
       <h1>Users List</h1>
