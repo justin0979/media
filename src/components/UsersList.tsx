ee9bf13fd3393c54a4cdca5e11840b7e9579fc8d
justin0979 <justinm0979@gmail.com>
2023-06-26 18:52:19 -0500

    add addUser button and function

--- src/components/UsersList.tsx
+++ src/components/UsersList.tsx
@@ -17,7 +17,7 @@ function UsersList() {
   }, []);
 
   if (isLoading) {
-    return <Skeleton times={6} className="h-10 w-full" />;
+    return <Skeleton times={data.length} className="h-10 w-full" />;
   }
 
   if (error) {
