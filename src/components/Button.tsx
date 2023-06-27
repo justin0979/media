ea5fe7687e6d36d061541f19ac945db72acba232
justin0979 <justinm0979@gmail.com>
2023-06-27 16:02:14 -0500

    refactor  button to show spinner when adding users

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from "react";
 import className from "classnames";
+import { GoSync } from "react-icons/go";
 
 interface ButtonProps {
   children: ReactNode;
@@ -10,6 +11,7 @@ interface ButtonProps {
   danger?: boolean;
   outline?: boolean;
   rounded?: boolean;
+  loading: boolean;
   [rest: string]: any;
 }
 
@@ -22,12 +24,14 @@ function Button({
   danger,
   outline,
   rounded,
+  loading,
   ...rest
 }: ButtonProps) {
   const classes = className(
     rest.className,
-    "flex items-center px-3 py-1.5 border",
+    "flex items-center px-3 py-1.5 border h-8",
     {
+      "opacity-80": loading,
       "border-blue-500 bg-blue-500 text-white": primary,
       "border-gray-900 bg-gray-900 text-white": secondary,
       "border-green-500 bg-green-500 text-white": success,
@@ -44,8 +48,8 @@ function Button({
   );
 
   return (
-    <button {...rest} className={classes}>
-      {children}
+    <button {...rest} disabled={loading} className={classes}>
+      {loading ? <GoSync className="animate-spin" /> : children}
     </button>
   );
 }
