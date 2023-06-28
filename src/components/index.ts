351033c40d85986dc398e0fa62ebae2ca918eeca
justin0979 <justinm0979@gmail.com>
2023-06-28 15:34:43 -0500

    export UsersListItem

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,6 @@ import Button from "./Button";
 import Panel from "./Panel";
 import Skeleton from "./Skeleton";
 import UsersList from "./UsersList";
+import UsersListItem from "./UsersListItem";
 
-export { Button, Panel, Skeleton, UsersList };
+export { Button, Panel, Skeleton, UsersList, UsersListItem };
