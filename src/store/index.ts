59e80544cb64139261e7d7ee4842cc070806cc7d
justin0979 <justinm0979@gmail.com>
2023-06-29 18:00:08 -0500

    hookup albumsApi

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,27 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
 import { usersReducer, UsersState } from "./slices";
+import { albumsApi } from "./apis";
 
 const store = configureStore({
   reducer: {
     users: usersReducer,
+    /*
+     *  instead of just using: albums: albumsApi.reducer
+     *  we use below just in case we have a typo either here or in the albumsApi
+     */
+    [albumsApi.reducerPath]: albumsApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware().concat(albumsApi.middleware);
   },
 });
+
+setupListeners(store.dispatch);
 
 // Inferred type for AppDispatch: { users: UsersState }
 export type AppDispatch = typeof store.dispatch;
 export type { UsersState };
 export * from "./thunks";
+export { useFetchAlbumsQuery } from "./apis";
 export { store };
