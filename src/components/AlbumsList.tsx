8d1ae1d3bad0d91a17b17943a809e22360381276
justin0979 <justinm0979@gmail.com>
2023-06-29 17:58:17 -0500

    display content for expandable panel

--- src/components/AlbumsList.tsx
+++ src/components/AlbumsList.tsx
@@ -1,12 +1,45 @@
 import { ReactNode } from "react";
 import { UsersType } from "../types";
+import { useFetchAlbumsQuery } from "../store";
+import Skeleton from "./Skeleton";
+import ExpandablePanel from "./ExpandablePanel";
+import Button from "./Button";
 
 interface AlbumsListProps {
   user: UsersType;
 }
 
 function AlbumsList({ user }: AlbumsListProps) {
-  return <div>Albums for {user.name}</div>;
+  /*
+   *  data is automatically retrieved without the need of useEffect hook.
+   *  isLoading is only true on initial fetch and will always be false afterwards.
+   */
+  const { data, error, isLoading } = useFetchAlbumsQuery(user);
+
+  let content: ReactNode;
+  if (data != undefined) {
+    if (isLoading) {
+      content = <Skeleton times={3} />;
+    } else if (error) {
+      content = <div>Error loading albums.</div>;
+    } else {
+      content = data.map((album) => {
+        const header = <div>{album.title}</div>;
+        return (
+          <ExpandablePanel key={album.id} header={header}>
+            Album's Photos Content
+          </ExpandablePanel>
+        );
+      });
+    }
+  }
+
+  return (
+    <div>
+      <div>Albums for {user.name}</div>
+      <div>{content}</div>
+    </div>
+  );
 }
 
 export default AlbumsList;
