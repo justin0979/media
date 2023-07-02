745b6901a8364a96912875677b056d16d19d4117
justin0979 <justinm0979@gmail.com>
2023-07-02 18:31:13 -0500

    add styling to photos list

--- src/components/PhotosList.tsx
+++ src/components/PhotosList.tsx
@@ -42,7 +42,9 @@ function PhotosList({ album }: PhotosListProps) {
           + Add Photo
         </Button>
       </div>
-      <div>{content}</div>
+      <div className="mx-8 flex flex-row flex-wrap justify-center">
+        {content}
+      </div>
     </div>
   );
 }
--- src/components/PhotosListItem.tsx
+++ src/components/PhotosListItem.tsx
@@ -6,7 +6,7 @@ interface PhotosListItemProps {
 
 function PhotosListItem({ photo }: PhotosListItemProps) {
   return (
-    <div>
+    <div className="relative m-2">
       <img className="h-20 w-20" src={photo.url} alt="Random pic" />
     </div>
   );
